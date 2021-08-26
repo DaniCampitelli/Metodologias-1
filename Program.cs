9401ba19f59bfdbf6329f0d5c56a8e9e1064b4dd
daniel campitelli <campitelligustavo@gmail.com>
2021-08-26 01:04:18 -0300

    Primer Commit

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace Metodologias_1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Probando");
         }
     }
 }
