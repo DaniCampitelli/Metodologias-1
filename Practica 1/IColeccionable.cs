8fd3ee73e70b06735173014285cf508e5207c213
daniel campitelli <campitelligustavo@gmail.com>
2021-08-31 23:50:00 -0300

    Implementacion ej 1-4

---
+++ Practica 1/IColeccionable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metodologias_1.Practica_1
+{
+    interface IColeccionable
+    {
+        public int cuantos();
+        public int minimo();
+        public int maximo();
+        public void agregar(Icomparable a);
+        public bool contiene(Icomparable a);
+    }
+}
---
+++ Practica 1/Numero.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metodologias_1.Practica_1
+{
+    class Numero:Icomparable
+    {
+        int valor;
+        public Numero(int n)
+        {
+            this.valor = n;
+        }
+        public int getValor ()
+        {
+            return valor;
+        }
+
+        public bool sosIgual(Icomparable a)
+        {
+
+            return ((Numero)a).valor == valor;
+        }
+
+        public bool sosMayor(Icomparable a)
+        {
+            return ((Numero)a).valor < valor;
+        }
+
+        public bool sosMenor(Icomparable a)
+        {
+            return ((Numero)a).valor > valor;
+        }
+    }
+}
